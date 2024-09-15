c56be3a26c98c4909d64e1aee2067f27f3f51bea
cetinbas <cetinbas@gmail.com>
2024-09-15 18:05:41 +0400

    added Ignore (in the model but not on the form) and Hidden (on the form but not visible) attributes

---
+++ src/QuickForm/Attributes/HiddenAttribute.cs
@@ -0,0 +1,9 @@
+﻿namespace QuickForm.Attributes;
+
+/// <summary>
+/// For attributes that have an input field, but should not be displayed.
+/// </summary>
+[AttributeUsage(AttributeTargets.Property)]
+public class HiddenAttribute : Attribute
+{
+}
---
+++ src/QuickForm/Attributes/IgnoreAttribute.cs
@@ -0,0 +1,9 @@
+﻿namespace QuickForm.Attributes;
+
+/// <summary>
+/// For attributes that don't have NotMapped, but still need to be ignored.
+/// </summary>
+[AttributeUsage(AttributeTargets.Property)]
+public class IgnoreAttribute : Attribute
+{
+}
